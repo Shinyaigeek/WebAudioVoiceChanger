f116b3ae5486df269b0b9aea1e2f205e30f2c4b4
svpcadmin <PhilisPaxil@gmail.com>
2019-12-06 18:15:17 +0900

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: {
+    bundle: "./src/app.tsx"
+  },
+  output: {
+    path: path.join(__dirname, "dist"),
+    filename: "[name].js"
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".json"]
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "dist")
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: "ts-loader"
+      },
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"]
+      }
+    ]
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+        template: './src/index.html'
+    })
+],
+};
